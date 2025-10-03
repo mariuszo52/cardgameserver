f503aebc629a27335762130dd4fbdce703feb8ba
mariuszo52 <the506miz@gmail.com>
2025-10-03 10:16:00 +0200

    initial commit

---
+++ src/main/java/com/cardgame/cardgameserver/CardgameserverApplication.java
@@ -0,0 +1,13 @@
+package com.cardgame.cardgameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgameserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardgameserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardgame/cardgameserver/CardgameserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardgame.cardgameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgameserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
